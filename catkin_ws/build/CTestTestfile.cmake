0dbf2c3339cc91de1d03a8e3c137da8d52ec8e55
Pawel R <48156138+miodine@users.noreply.github.com>
2022-12-27 12:17:34 +0100

    strategy one

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/miodine/Desktop/mission_coordination_tp/catkin_ws/src
+# Build directory: /home/miodine/Desktop/mission_coordination_tp/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("Mission_Coordination_project/evry_project_strategy")
+subdirs("Mission_Coordination_project/evry_project_description")
+subdirs("Mission_Coordination_project/evry_project_plugins")
